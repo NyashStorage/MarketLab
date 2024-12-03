cb3224ca14ec86e9eb607b0052b541a2270ce14c
Daniil Koshkin <admin@nyashmyash99.dev>
2024-12-04 00:31:55 +0300

    Feature | Расширены правила prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/**
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  // tabWidth: 2,
+  // semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  // bracketSpacing: true,
+  // bracketSameLine: false,
+  // arrowParens: 'always',
+}
